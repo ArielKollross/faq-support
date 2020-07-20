429ed873d0ed3df997084e60679ecd00eaad9de2
Ariel Kollross <arielkollross@gmail.com>
2020-07-20 09:02:29 -0300

    changed backend db and structore

--- backend/src/database/connection.js
+++
@@ -1,6 +0,0 @@
-//configurando a conexao com o bado de dados
-const knex = require('knex')
-const configurationDb = require('../../knexfile')
-const connectionDb = knex(configurationDb.development)
-
-module.exports = connectionDb
